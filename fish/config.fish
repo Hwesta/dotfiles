164d9a2f281fad22958bdb33b531c01ac7798e6e
Holly Becker <becker.holly@gmail.com>
2023-04-20 10:38:05 -0700

    Remove unused IBus & Ruby config

--- fish/config.fish
+++ fish/config.fish
@@ -13,11 +13,6 @@ if status --is-interactive
 	eval (ssh-agent -c)
 end
 
-# IBus
-set -xg GTK_IM_MODULE ibus
-set -xg XMODIFIERS @im=ibus
-set -xg QT_IM_MODULE ibus
-
 # Git pager
 set -xg LESS eFRX
 
@@ -26,5 +21,3 @@ set -xg NPM_PACKAGES $HOME/.npm-packages
 set -xg NODE_PATH $NPM_PACKAGES/lib/node_modules $NODE_PATH
 set -xg PATH $NPM_PACKAGES/bin $PATH
 
-# Ruby
-set -xg PATH (ruby -e 'print Gem.user_dir')/bin $PATH
