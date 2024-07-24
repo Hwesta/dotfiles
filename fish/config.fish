71dae1f128388726b82502a2d726afab2067b3ec
Holly Becker <becker.holly@gmail.com>
2024-07-23 18:54:20 -0700

    fish python: disable virtualfish, enable pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,15 @@ set -xg EDITOR '/usr/bin/vim'
 # Git pager
 set -xg LESS eFRX
 
+# Pyenv
+pyenv init - | source
+
+# 2024-05-20 virtualfish not installed/not working for a while, disable
 # VirtualFish - fish virtualenvwrapper
 # Use same virtualenv location as pipenv
-set -xg VIRTUALFISH_HOME ~/.local/share/virtualenvs
+# set -xg VIRTUALFISH_HOME ~/.local/share/virtualenvs
 # pipenv completions
-eval (env _PIPENV_COMPLETE=fish_source pipenv)
+# eval (env _PIPENV_COMPLETE=fish_source pipenv)
 # disable double prompt - if you do this, uncomment virtualenv in fish_prompt
 # set -xg VIRTUAL_ENV_DISABLE_PROMPT 1
 
